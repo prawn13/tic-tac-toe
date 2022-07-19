9c0fc88eca242179a0ff4991f87cea4179d29aa5
penelope davis <penelopesdavis@gmail.com>
2022-07-19 11:57:00 -0400

    Try to start but confused

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
-let board = (() => {
-    let board = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]
-    ];
-
-    function move(player, space) {
-        let x = space % 3;
-        let y = Math.floor((space - 1) / 3);
-        board[y][x] = player;
+const player = ((marker, name) => {
+    let _marker = marker;
+    let _name = name;
+    const getMarker = () => {
+        return _marker;
+    } 
+    const getName = () => {
+        return _name;
     }
+    return {
+        getMarker,
+        getName
+    }
+});
 
-    function render() {
-        const boardDiv = document.getElementById('board');
-        
+let board = (() => {
+    let _board = new Array(9).fill("_");
+    let _turn = "X";
+    
+    function move(space) {
+        if (_board[space] !== " ") return;
+        _board[space] = _turn;
+        _turn = (_turn === "X") ? "O" : "X";
+        displayController.render();
     }
 
     return {
-        move: move
+        move: move,
+        
     };
 })();
+
+
+let displayController = (() => {
+    function init() {
+        let _p1 = player("X");
+        let _p2 = player("O");
+
+        const spaces = document.querySelectorAll('.space');
+        for(let i = 0; i < 9; i++) {
+            console.log(spaces[i]);
+        }
+    }
+
+    function render() {
+        const spaces = document.querySelectorAll('.space');
+        for(let i = 0; i < 9; i++) {
+            spaces[i].innerHTML = board[i];
+            // console.log(spaces[i]);
+        }
+    }
+
+    init();
+    return {
+        render
+    }
+
+})();
+
+
+// board.init();
